d894b44c20232711c4c19fb81b9e7a9f0138890c
Jonathan Moallem <jdmoallem@gmail.com>
2020-04-05 15:27:37 +1000

    Created bouncing, autonomous nodes that can infect each other

---
+++ node.gd
@@ -0,0 +1,24 @@
+extends RigidBody2D
+
+export (bool) var infected
+
+func _ready():
+	$Timer.connect("timeout", self, "initiate_movement")
+	connect("body_entered", self, "infect_other")
+	if infected:
+		infect()
+
+func initiate_movement():
+	$Timer.wait_time = rand_range(5, 10)
+	apply_impulse(Vector2.ZERO, Vector2(rand_range(-100, 100), rand_range(-100, 100)))
+
+func _process(delta):
+	$Sprite.global_rotation_degrees = 0
+
+func infect():
+	infected = true
+	$Sprite.modulate = Color(1, 0, 0)
+
+func infect_other(other):
+	if infected and other is RigidBody2D:
+		other.infect()
