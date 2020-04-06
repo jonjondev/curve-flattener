8cc8f1ab7982a976b83ffb8f58db4c5d0e9c68b0
Jonathan Moallem <jdmoallem@gmail.com>
2020-04-06 12:00:34 +1000

    Added immunity to nodes

--- node.gd
+++ node.gd
@@ -1,26 +1,35 @@
 extends RigidBody2D
 
 export (bool) var infected
+export (bool) var immune
 
 func _ready():
-	$Timer.connect("timeout", self, "initiate_movement")
+	$MoveTimer.connect("timeout", self, "initiate_movement")
 	connect("body_entered", self, "infect_other")
 	if infected:
 		infect()
 
 func initiate_movement():
-	$Timer.wait_time = rand_range(5, 10)
+	$MoveTimer.wait_time = rand_range(5, 10)
 	apply_impulse(Vector2.ZERO, Vector2(rand_range(-100, 100), rand_range(-100, 100)))
 
 func _process(delta):
 	$Sprite.global_rotation_degrees = 0
 
-func infect():
-	infected = true
-	$Sprite.modulate = Color(1, 0, 0)
-	add_to_group("infected")
-	
-
 func infect_other(other):
 	if infected and other is RigidBody2D:
 		other.infect()
+
+func infect():
+	if not immune:
+		infected = true
+		$Sprite.modulate = Color(1, 0, 0)
+		add_to_group("infected")
+		get_tree().create_timer(rand_range(14, 42)).connect("timeout", self, "recover")
+
+func recover():
+	infected = false
+	immune = true
+	$Sprite.modulate = Color(0, 1, 0)
+	remove_from_group("infected")
+	add_to_group("immune")
