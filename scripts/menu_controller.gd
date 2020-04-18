f465865814233ace5bd89ca53284bbd16d60e54c
Jonathan Moallem <jdmoallem@gmail.com>
2020-04-18 22:37:35 +1000

    Added ability to take screenshots and crop resulting textures before save

--- scripts/menu_controller.gd
+++ scripts/menu_controller.gd
@@ -5,3 +5,12 @@ func _ready():
 
 func play():
 	get_tree().change_scene("res://scenes/main.tscn")
+
+func _input(event):
+	if event is InputEventKey and event.scancode == KEY_SPACE:
+		var img = get_viewport().get_texture().get_data()
+		img.flip_y()
+		img.flip_x()
+		img.crop(100, 100)
+		img.flip_x()
+		img.save_png("user://screenshot.png")
