bc02b6409362db37d8d955f3585d05d8320c3082
Jonathan Moallem <jdmoallem@gmail.com>
2020-04-20 16:59:18 +1000

    Moved screenshot code to execute in the endgame scene once the first frame has run

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -6,3 +6,13 @@ func _ready():
 	$Graph.anchor_top = 0.1
 	$Graph.anchor_right = 0.9
 	$Graph.anchor_bottom = 0.9
+	
+	yield(VisualServer, 'frame_post_draw')
+	
+	var viewport_size = get_viewport().size
+	var img = get_viewport().get_texture().get_data()
+	img.flip_y()
+	img.flip_x()
+	img.crop(viewport_size.x/2, viewport_size.y)
+	img.flip_x()
+	img.save_png("user://screenshot.png")
--- scripts/menu_controller.gd
+++ scripts/menu_controller.gd
@@ -2,12 +2,3 @@ extends Control
 
 func _ready():
 	$PlayButton.connect("button_up", SceneManager, "start_game")
-
-func _input(event):
-	if event is InputEventKey and event.scancode == KEY_SPACE:
-		var img = get_viewport().get_texture().get_data()
-		img.flip_y()
-		img.flip_x()
-		img.crop(100, 100)
-		img.flip_x()
-		img.save_png("user://screenshot.png")
